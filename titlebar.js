557de0db44d3efdebc7af055ad44073f15f75801
Treidex <treidexwastaken@gmail.com>
2021-11-15 21:29:11 -0800

    skool games

--- titlebar.js
+++ titlebar.js
@@ -5,6 +5,7 @@ const titlebarHtml =
 	<a href="/"><img src="/icon.png" width="60px"></a>
 	<a target="_blank" href="https://github.com/TeddyTelanoff"><h3>Github</h3></a>
 	<a href="/games"><h3>Games</h3></a>
+	<a href="https://editor.p5js.org/SkoolBoiTreidex/full/azR7YeMhG"><h3>School Games</h3></a>
 	<a href="/devlog" id="devlog"><h3>DevLog</h3></a>
 `;
 
