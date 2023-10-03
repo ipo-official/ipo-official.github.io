91db8500837a7cefd122a153d4399842793d2e2e
Treidex <29633838+Treidexy@users.noreply.github.com>
2023-10-02 20:43:22 -0700

    Update titlebar.js

--- titlebar.js
+++ titlebar.js
@@ -6,6 +6,7 @@ const titlebarHtml =
 	<a target="_blank" href="https://github.com/TeddyTelanoff"><h3>Github</h3></a>
 	<a href="/games"><h3>Games</h3></a>
 	<a href="https://editor.p5js.org/SkoolBoiTreidex/full/azR7YeMhG"><h3>School Games</h3></a>
+ 	<a href="/desmos"><h3>Desmos Games</h3></a>
 	<a href="/devlog" id="devlog"><h3>DevLog</h3></a>
 `;
 
